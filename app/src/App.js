7317440dde61cf1559a1aaecbf6f306cfb195d71
DaviAieta <aieta.davi@gmail.com>
2024-04-02 17:11:56 -0300

    Update list component

--- app/src/App.js
+++ app/src/App.js
@@ -4,11 +4,11 @@ import PersonList from './components/list'
 import './App.css'
 
 function App() {
-  const people = {
-    1: {name: 'Alice'},
-    2: {name: 'Bob'},
-    3: {name: 'Davi'}
-  }
+  const people = [
+  { name: 'Alice' },
+  { name: 'Bob' },
+  { name: 'Davi' }
+  ]
 
   const user = 'Davi'
 
